8e400a86e5d5ed60e4421f47ec6236415a5893bf
saurabhbahl19@gmail.com <ec2-user@ip-172-31-31-19.us-east-2.compute.internal>
2019-05-07 21:30:21 +0000

    new back colwor

--- app/index.php
+++ app/index.php
@@ -20,7 +20,7 @@ $cities = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <title>Saurabh you Rock!</title>
 </head>
-<body style="background-color:#FF00FF;">
+<body style="background-color:#FF0000;">
     <h2>Most Populous Cities In The World</h2>
     <table>
     <thead>

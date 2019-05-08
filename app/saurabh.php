6329ed2066d5513636c1082a9d2961ee1d483e19
saurabhbahl19@gmail.com <ec2-user@ip-172-31-31-19.us-east-2.compute.internal>
2019-05-08 03:29:34 +0000

    add missing file

---
+++ app/saurabh.php
@@ -0,0 +1,50 @@
+CTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<LEFT>
+     <h1><FONT COLOR = #FF0033>Welcome page for my App</FONT> </h1>
+<h2>Docker Container Runing </h2>
+
+<table>
+  <tr>
+    <th>Container</th>
+    <th>Version</th>
+  </tr>
+  <tr>
+    <td>PHP</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Ngingx</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>MySql</td>
+    <td>-</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
+
